e4bc73657799a33892146e2608e4439dcca97a91
rubi580 <hide72861@gmail.com>
2025-04-22 12:19:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-
+document.getElementById("addButton").addEventListener("click", function () {
+    const input = document.getElementById("newMemoInput");
+    const text = input.value.trim();
+    if (text === "") return;
+  
+    const li = document.createElement("li");
+    li.classList.add("memo");
+  
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+  
+    const span = document.createElement("span");
+    span.textContent = text;
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "🌼";
+    deleteButton.classList.add("delete");
+    deleteButton.addEventListener("click", function () {
+      li.remove();
+    });
+  
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+  
+    document.getElementById("memoList").appendChild(li);
+    input.value = "";
+  });
+  
